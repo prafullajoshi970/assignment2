8040b7b1705ba456f42a4b6d86270d1e52a711de
prafullajoshi970 <97543959+prafullajoshi970@users.noreply.github.com>
2022-11-01 22:08:26 +0530

    first Commmit

---
+++ src/components/Main.js
@@ -0,0 +1,27 @@
+function Main() {
+    return (
+     <div>
+      <div className="App">
+      <div className="first"> 
+        <h1>
+          The Evolution of
+        </h1>
+         <h1>
+           Airbnb's Frontend
+         </h1>
+      
+      </div>
+      <div className="second">
+        <h6>@spikebrehum</h6>
+        <h6>airbnb</h6>
+  
+         
+      </div>
+    
+    </div>
+      </div>
+    );
+  }
+  
+  export default Main;
+  
